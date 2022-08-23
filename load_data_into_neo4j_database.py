a4c5f0933cae4d896a04bcf4e178db2a735166f8
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-23 17:56:39 +0200

    Update load_data_into_neo4j_database.py

--- load_data_into_neo4j_database.py
+++ load_data_into_neo4j_database.py
@@ -34,7 +34,7 @@ print('done')
 print('Inserting edges')
 
 query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/main/edges.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/d1535dfe3bc047326e5c264b1a5249798f3120e9/edges.csv' AS row
 CREATE (:Edge {hero: row.hero, comic: row.comic});
 '''
 
