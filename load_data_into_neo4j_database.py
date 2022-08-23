94f4e1886dd62d7146036ccc40a36a1134d340c0
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-23 16:39:12 +0200

    Create load_data_into_neo4j_database.py

---
+++ load_data_into_neo4j_database.py
@@ -0,0 +1,60 @@
+from neo4j import GraphDatabase
+
+driver = GraphDatabase.driver('bolt://0.0.0.0:7687',
+                              auth=('neo4j', 'neo4j'))
+
+# deleting data
+print('Deleting previous data')
+
+query = '''
+MATCH (n)
+DETACH DELETE n
+'''
+
+with driver.session() as session:
+    print(query)
+    session.run(query)
+
+print('done')
+
+# inserting data
+print('Inserting nodes')
+
+query = '''
+LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/main/nodes.csv' AS row
+CREATE (:Node {name: row.node, type: row.type});
+'''
+
+with driver.session() as session:
+    print(query)
+    session.run(query)
+
+print('done')
+
+print('Inserting edges')
+
+query = '''
+LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/main/edges.csv' AS row
+CREATE (:Edge {hero_name: row.hero, comic_name: row.comic});
+'''
+
+with driver.session() as session:
+    print(query)
+    session.run(query)
+
+print('done')
+
+print('Creating relationships')
+
+query = '''
+LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/main/hero-network.csv' AS row
+MATCH (n:Node) WHERE n.name = row.hero1
+MATCH (e:Edge) WHERE e.hero_name = row.hero2
+CREATE (n.name)-[:APPEAR_IN]->(e.comic_name)<-[:APPEAR_IN]-(e.hero_name)
+'''
+
+with driver.session() as session:
+    print(query)
+    session.run(query)
+
+print('done')
