fff5c2fcf6904353d86a60168fdbd77cd87476fb
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-24 22:14:41 +0200

    Update load_data_into_neo4j_database.py

--- load_data_into_neo4j_database.py
+++ load_data_into_neo4j_database.py
@@ -21,7 +21,7 @@ print('done')
 print('Inserting heroes')
 
 query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/4eda7574890aca72f304a7e7a874da2715208a40/hero.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/ZGabriello/Project3-Databases-API/main/hero.csv' AS row
 CREATE (:Hero {name: row.node});
 '''
 
@@ -34,34 +34,8 @@ print('done')
 print('Inserting Comics')
 
 query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/4eda7574890aca72f304a7e7a874da2715208a40/comic.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/ZGabriello/Project3-Databases-API/main/comic.csv' AS row
 CREATE (:Comic {name: row.node});
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-print('Inserting edges')
-
-query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/d1535dfe3bc047326e5c264b1a5249798f3120e9/edges.csv' AS row
-CREATE (:Edge {hero: row.hero, comic: row.comic});
-'''
-
-with driver.session() as session:
-    print(query)
-    session.run(query)
-
-print('done')
-
-print('Inserting hero_network')
-
-query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/0d8ddaa6365fb10974d9c5cf9e3d0f09962e552f/hero-network.csv' AS row
-CREATE (:Same_comic {hero1: row.hero1, hero2: row.hero2});
 '''
 
 with driver.session() as session:
@@ -73,7 +47,7 @@ print('done')
 print('Creating Relationships')
 
 query = '''
-LOAD CSV WITH HEADERS FROM 'https://github.com/ZGabriello/Project3-Databases-API/blob/d1535dfe3bc047326e5c264b1a5249798f3120e9/edges.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/ZGabriello/Project3-Databases-API/main/edges.csv' AS row
 MATCH (h:Hero) WHERE h.name = row.hero
 MATCH (c:Comic) WHERE c.name = row.comic
 CREATE (h)-[:APPEAR_IN]->(c);
