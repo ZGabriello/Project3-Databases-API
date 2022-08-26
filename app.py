426570963e472c8bb9e3f124f78adfe7336012df
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-26 14:46:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import request
+from neo4j import GraphDatabase
 
 app = Flask(__name__)
 
 // Neo4j database 
 driver = GraphDatabase.driver("bolt://52.48.111.88:7687", auth=basic_auth("neo4j", "neo4j"))
+driver = GraphDatabase.driver(uri="bolt://52.48.111.88:7687", auth=("neo4j", "neo4j"))
+session = driver.session()
 
+query = """
+match (n:Hero) return n.name as name limit 10
+"""
+results = session.run(query)
+
+for result in results:
+    print(result["name"])
 
 @app.route("/")
 def welcome():
     return "<h1> Welcome to the API that manage requests to our Neo4J BDD !  </h1>"
+
+@app.route("/heroes")
+def list_heroes():
+    return "La liste des heros de comics est : {}".format(results)
   
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
