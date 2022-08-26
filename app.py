ebe114f373751922c8bff8abf7d3809d9678da0d
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-26 13:37:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import request
 
 app = Flask(__name__)
 
+// Neo4j database 
+driver = GraphDatabase.driver("bolt://52.48.111.88:7687", auth=basic_auth("neo4j", "neo4j"))
+
+
 @app.route("/")
 def welcome():
     return "<h1> Welcome to the API that manage requests to our Neo4J BDD !  </h1>"
