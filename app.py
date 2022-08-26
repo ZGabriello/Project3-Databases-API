cb3c2df3843792e307d457c194fbc0cd59503c91
Gabriello ZAFIFOMENDRAHA <gabriello.zafifomendraha@gmail.com>
2022-08-26 17:48:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,24 @@ def list_comics():
         <h3> La liste des comics est : </h3>
         {}
         """.format(data)
+
+@app.route("/hero_appearing_in_comic")
+def hero_and_comic():
+    query = """
+    match (n)-[:APPEAR_IN]->(p) return n.name as hero,p.name as comic limit 10 
+    """
+    results = session.run(query)
+    data_hero = []
+    data_comic = []
+    data
+    for result in results:
+        data_hero.append(result["hero"])
+        data_comic.append(result["comic"])
+        data.append([data_hero,data_comic])
+    return """
+        <h3> La liste des comics est : </h3>
+        {}
+        """.format(data)
   
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
